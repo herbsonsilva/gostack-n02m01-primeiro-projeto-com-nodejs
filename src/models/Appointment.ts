42695546f131d9e0ab28b97ddfbb33c0a9d7e38d
Herbson Silva <herbsonsilvajp@gmail.com>
2021-03-18 22:33:29 -0300

    Add repositories and services

---
+++ src/models/Appointment.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid';
+
+interface AppointmentConstructor {
+  provider: string;
+  date: Date;
+}
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    // if (!this.id) {
+    this.id = uuid();
+    // }
+
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
